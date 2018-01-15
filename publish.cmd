dc2b4e55d54a42a6541ec949bda2000902a651b5
Qinen Zhu <qinzhu@microsoft.com>
2018-01-15 18:49:23 +0800

    Add publish script

---
+++ publish.cmd
@@ -0,0 +1,23 @@
+rmdir /s /q output
+
+dotnet publish --self-contained -r win-x86 -c release -o output\win32_x86
+dotnet publish --self-contained -r win-x64 -c release -o output\win32_x64
+dotnet publish --self-contained -r linux-x64 -c release -o output\linux_x64
+dotnet publish --self-contained -r osx-x64 -c release -o output\darwin_x64
+
+cd output
+
+cd win32_x86
+..\..\7z.exe a ..\win32_x86.zip *
+cd ..
+cd win32_x64
+..\..\7z.exe a ..\win32_x64.zip *
+cd ..
+cd linux_x64
+..\..\7z.exe a ..\linux_x64.zip *
+cd ..
+cd darwin_x64
+..\..\7z.exe a ..\darwin_x64.zip *
+
+cd ..
+cd ..
