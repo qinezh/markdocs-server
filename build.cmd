08df65eef8d0a15e397552d7a57809b0e407671f
Qinen Zhu <qinzhu@microsoft.com>
2018-06-22 15:27:12 +0800

    Update build script to publish release based on netcore

--- build.cmd
+++ build.cmd
@@ -1,5 +1,6 @@
 rmdir /s /q output
 
+dotnet publish -c release -o output\netcore
 dotnet publish --self-contained -r win-x86 -c release -o output\win32_x86
 dotnet publish --self-contained -r win-x64 -c release -o output\win32_x64
 dotnet publish --self-contained -r linux-x64 -c release -o output\linux_x64
